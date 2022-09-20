2d86697aca3047cc67a57203d372c5080819b9a2
Rimoczi Daniel <rimoczidani007@gmail.com>
2022-09-20 12:58:29 +0200

    Feladat3_kesz

--- src/lista.js
+++ src/lista.js
@@ -1,17 +1,19 @@
+
 let lista = []
 export function hozzad(szam) {
     if (szam < 0) {
         console.log("Hiba")
     }
     else  {
-        lista.push(szak)
+        lista.push(szam)
     }
 }
 export function atlag() {
     let atlag = 0
     for (let i = 0; i < lista.length; i++) {
         atlag += lista[i];
-        return atlag / lista.length;
+        
         
     }
+    return atlag / lista.length;
 }
---
+++ src/test.js
@@ -0,0 +1,15 @@
+import { hozzad, atlag } from './lista.js';
+
+hozzad(5)
+console.log( atlag());
+hozzad(10)
+console.log( atlag());
+hozzad(561)
+console.log( atlag());
+hozzad(30)
+console.log( atlag());
+hozzad(40)
+console.log( atlag());
+hozzad(66)
+console.log( atlag());
+hozzad(-66)
